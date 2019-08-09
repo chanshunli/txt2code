4a0e9d5af3c5441ffa00bbc3289a60602c90b04e
chanshunli <chanshunli@gmail.com>
2019-08-09 11:32:28 +0800

    Txt2Code hello world出来了

--- src/txt2code/core.cljs
+++ src/txt2code/core.cljs
@@ -1,5 +1,6 @@
 (ns txt2code.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require
+   [reagent.core :as reagent :refer [atom]]))
 
 (def blockly (js* "Blockly"))
 (def blockly-js (js* "Blockly.JavaScript"))
@@ -7,26 +8,77 @@
 (def blockly-xml (js* "Blockly.Xml"))
 (def blockly-blocks (js* "Blockly.Blocks"))
 
-;; define your app data so that it doesn't get over-written on reload
+(def workspace (reagent/atom {}))
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn get-e [s]
+  (js/document.getElementById s))
 
-(defn hello-world []
+(defn text->dom
+  [xml-str]
+  (.textToDom blockly-xml xml-str))
+
+(defn append-dom->workspace
+  [dom workspace]
+  (.appendDomToWorkspace blockly-xml dom workspace))
+
+(defn get-element-by-id
+  [ename]
+  (.getElementById js/document "blockly"))
+
+(defn dom->workspace
+  [saved-blocks workspace]
+  (let [xml (.textToDom blockly-xml saved-blocks)]
+    (.domToWorkspace blockly-xml xml workspace)))
+
+(defn block->dom
+  [block]
+  (.blockToDom blockly-xml block))
+
+(def blockly-style {:height 600
+                    :width "100%"})
+
+(defn generate-js-button []
+  [:button {:id "generate-js"
+            :type "button"
+            :on-click #(let [code (.workspaceToCode blockly-js @workspace)]
+                         (if (empty? code)
+                           (js/alert "请输入blockly代码(*@ο@*) 哇～")
+                           (js/alert code)))}
+   "生成JavaScript"])
+
+(defn blockly-semantic-input-method []
+  [:form {:class "form", :novalidate ""}
+   [:div {:class "form-group row no-label"}
+    [:label {:class "control-label col-sm-3", :data-required "false", :for "frc-q-1088"}]
+    [:div {:class "col-sm-9"}
+     [:input {:type "text", :class "input", :aria-label "搜索", :name "q", :placeholder "输入文本生成代码", :id "frc-q-1088", :value ""}]
+     [:span {:class "help-block"
+             :style {:font-size "25px"}}
+      "Txt2Code"]
+     (generate-js-button)]]])
+
+(defn ui
+  []
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   [:div#menu {:style {:position "relative"
+                       :top 0
+                       :height 100
+                       :width "100%"}}
+    [:center (blockly-semantic-input-method)]]
+   [:div#blocklyDiv {:style blockly-style}]])
 
 (defn start []
-  (reagent/render-component [hello-world]
-                            (. js/document (getElementById "app"))))
+  (js/console.log "start...")
+  (reagent/render
+   [ui]
+   (get-e "app"))
+  (reset! workspace
+          (.inject blockly
+                   (get-e "blocklyDiv")
+                   (clj->js {:toolbox (get-e "toolbox")}))))
 
 (defn ^:export init []
-  ;; init is called ONCE when the page loads
-  ;; this is called in the index.html and must be exported
-  ;; so it is available even in :advanced release builds
   (start))
 
 (defn stop []
-  ;; stop is called before any code is reloaded
-  ;; this is controlled by :before-load in the config
-  (js/console.log "stop"))
+  (js/console.log "stop..."))
